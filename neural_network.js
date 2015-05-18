35abaa176879164333a3c9865b21806a27f33729
Andres <andres_m113@hotmail.com>
2015-05-17 22:42:00 -0600

    toByteArray now supports alpha transparency

--- neural_network.js
+++ neural_network.js
@@ -130,7 +130,7 @@ function back_propagation(){
 function toByteArray(input){
     var result = [];
     for(var i = 0; i < input.length; i+=4){
-        if(input[i] == 255){
+        if((input[i] === 255) || (input[i+3] === 0)){
             result.push(0);
         }else{
             result.push(1);
